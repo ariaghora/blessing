603c5b57039610859870d4bc5d132bd4c742e916
Aria Ghora Prabono <hello@ghora.net>
2021-12-15 07:45:56 +0700

    Single layer seems to be good

--- blessing/blessing.py
+++ blessing/blessing.py
@@ -5,17 +5,18 @@ from sklearn.preprocessing import MinMaxScaler
 
 
 class Blessing:
-    def __init__(self, k: int, max_iter: int = 200, scale_input: bool = True):
+    def __init__(self, k: int, max_iter: int = 300, scale_input: bool = True):
         self.k = k
         self.max_iter = max_iter
         self.scale_input = scale_input
 
-        self.hidden_layer_sizes = (100, 50, 100)
+        self.hidden_layer_sizes = (150,)
         self.ae: BaseEstimator = None
         self.column_scores: np.ndarray = None
         self.chosen_column_idx: np.ndarray = None
 
     def fit(self, X: np.ndarray, y=None, n_refine: int = 0):
+        X_ori = X.copy()
         """'Nudge' the values of constant columns"""
         X = X + (
             np.random.randn(*X.shape) * 10 ** (np.ceil(np.log10(np.abs(X) + 1e-8)) - 2)
@@ -42,6 +43,7 @@ class Blessing:
         col_mse = ((X_hat - X) ** 2).mean(0)
         col_penalty = np.log(X.var(0) + 1e-8)
         self.col_scores = 1 / (col_mse - col_penalty)
+        self.col_scores[X_ori.var(0) == 0] = 0
         self.chosen_column_idx = np.argsort(self.col_scores)[-self.k :][::-1]
         return self
 
